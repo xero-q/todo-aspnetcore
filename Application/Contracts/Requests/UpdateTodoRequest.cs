8693e6c39418d05901e1ec34d8fa198cc4ad965c
Anibal Sanchez <anibalnuma@gmail.com>
2025-10-30 12:39:39 -0400

    Removed unused code.

--- Application/Contracts/Requests/UpdateTodoRequest.cs
+++ Application/Contracts/Requests/UpdateTodoRequest.cs
@@ -2,8 +2,6 @@ namespace Application.Contracts.Requests;
 
 public sealed class UpdateTodoRequest
 {
-    public required int TodoId { get; init; }
-    
     public required string Title { get; init; }
     
     public required string Description { get; init; }
