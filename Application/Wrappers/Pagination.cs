b3e7a91fbd6a0ab807e624aedb20284db0d96b8d
Anibal Sanchez <anibalnuma@gmail.com>
2025-11-02 09:58:03 -0500

    Remove unused code.

--- Application/Wrappers/Pagination.cs
+++ Application/Wrappers/Pagination.cs
@@ -2,18 +2,6 @@
 
 public class Pagination<T>
 {
-    public Pagination()
-    {
-    }
-
-    public Pagination(int currentPage, int pageSize, int totalItems, List<T> result)
-    {
-        CurrentPage = currentPage;
-        PageSize = pageSize;
-        TotalItems = totalItems;
-        Result = result;
-        TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
-    }
     public int CurrentPage { get; set; }
 
     public int PageSize { get; set; }
